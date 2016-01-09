e691aed5d6d4bc99e98afe51962b4200b45969bc
Ourai Lin <ourairyu@hotmail.com>
2016-01-09 22:46:43 +0800

    添加部署到七牛的配置

---
+++ examples/webx/webapp/fis-conf.js
@@ -0,0 +1,47 @@
+// FIS 全局属性
+fis.set("project.md5Connector", "-");
+fis.set("project.ignore", [
+  "bower_components/**",
+  "node_modules/**",
+  "*-INF/**",
+  "fis-conf.js",
+  "package.json",
+  ".*"
+]);
+
+// 项目变量
+fis.set("components", "/home/templates/control/components");
+fis.set("qn_root", "/webapp_qiniu");
+
+// 开发调试
+fis
+  .media("dev")
+  .match("*.scss", {
+    rExt: ".css",
+    parser: fis.plugin("node-sass")
+  })
+  .match("*.vm", {
+    release: false
+  });
+
+// 部署到七牛
+fis
+  .media("qiniu")
+  .match("*.scss", {
+    release: false
+  }, true)
+  .match("*.{css,js,jpg,png,gif}", {
+    useHash: true
+  })
+  .match("**", {
+    release: "${qn_root}/$0",
+    domain: "//img.maihaoche.com/"
+  })
+  .match("${components}/(**/*.{css,js})", {
+    release: "${qn_root}/components/$1",
+    url: "components/$1"
+  })
+  .match("${components}/(**/*.{jpg,png,gif})", {
+    release: "${qn_root}/components/$1",
+    url: "components/$1"
+  });
