0761bcfd80bca211ef467b872d213de05b10a4da
Ourai Lin <ourairyu@hotmail.com>
2016-01-11 12:17:21 +0800

    添加部署到顽兔的配置

--- examples/webx/webapp/fis-conf.js
+++ examples/webx/webapp/fis-conf.js
@@ -2,6 +2,7 @@
 
 var rocketz = require("rocketz");
 var qiniuConf = rocketz.getCloud("qiniu");
+var wantuConf = rocketz.getCloud("wantu");
 
 // FIS 全局属性
 fis.set("project.md5Connector", "-");
@@ -17,6 +18,7 @@ fis.set("project.ignore", [
 // 项目变量
 fis.set("components", "/home/templates/control/components");
 fis.set("qn_root", "/webapp_qiniu");
+fis.set("wt_root", "/webapp_wantu");
 
 // 开发调试
 fis
@@ -70,4 +72,38 @@ fis
   });
 
 // 部署到顽兔
-// fis.media("prod_wantu");
+fis
+  .media("prod_wantu")
+  .match("*.scss", {
+    release: false
+  }, true)
+  .match("*.{css,js,jpg,png,gif}", {
+    useHash: true,
+    deploy: fis.plugin("wantu", {
+      accessKey: wantuConf.access_key,
+      secretKey: wantuConf.secret_key,
+      namespace: wantuConf.namespace
+    })
+  }, true)
+  .match("*.css", {
+    optimizer: fis.plugin("clean-css")
+  })
+  .match("*.js", {
+    optimizer: fis.plugin("uglify-js")
+  })
+  .match("*.png", {
+    optimizer: fis.plugin("png-compressor")
+  })
+  .match("**", {
+    release: "${wt_root}/$0",
+    domain: "//img1.maihaoche.com/",
+    deploy: fis.plugin("local-deliver")
+  })
+  .match("${components}/(**/*.{css,js})", {
+    release: "/components/$1",
+    url: "components/$1"
+  })
+  .match("${components}/(**/*.{jpg,png,gif})", {
+    release: "/components/$1",
+    url: "components/$1"
+  });
