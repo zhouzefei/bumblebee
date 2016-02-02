c5f340452761b63683f8af121b680a1c09e499f0
Ourai Lin <ourairyu@hotmail.com>
2016-02-02 18:50:53 +0800

    Integrate RocketZ to launch assets to clouds :-)

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 
 "use strict";
 
+var _ = require("lodash");
 var meow = require("meow");
 
 var cmd = require("../lib/command");
@@ -17,8 +18,16 @@ var cli = meow({
   });
 
 var args = cli.input;
-var opts = cli.flags;
 
+/**
+ * Initialization
+ *
+ * 1. Configure B3 itself;
+ * 2. Scaffold project with Yeoman generators
+ *
+ * Usage:
+ *    b3 init [generator-name]
+ */
 cmd.register("init", function( args, opts ) {
   var env;
 
@@ -33,7 +42,6 @@ cmd.register("init", function( args, opts ) {
     });
 
     env.lookup(function() {
-      // console.log(env.getGeneratorsMeta());
       env.run(args, opts);
     });
   }
@@ -43,4 +51,58 @@ cmd.register("init", function( args, opts ) {
   }
 });
 
-cmd.run(args[0], args.slice(1), opts);
+/**
+ * Uploading local assets to cloud (CDN)
+ *
+ * Usage:
+ *    b3 upload [flags]
+ *
+ * Flags:
+ *    - config        Configure cloud's info ('qiniu' & 'wantu' are available)
+ *    - assets        Local assets placed in (default is '.')
+ *    - remote        Target directory uploaded to (default is '.')
+ *    - files         Specify files' name to be uploaded (default is all)
+ *    - exts          Specify exts to be uploaded (default is all static assets)
+ *    - deep          Whether find files deeply (default is true)
+ *    - interactive   Whether interact with CLI (default is true)
+ */
+cmd.register("upload", function( args, opts ) {
+  var rocketz, conf;
+
+  if ( opts.config ) {
+    require("rocketz/lib/config").setConfig(opts.config);
+  }
+  else {
+    rocketz = require("rocketz");
+    conf = _.assign({
+        assets: ".",
+        remote: ".",
+        files: "",
+        exts: "",
+        deep: true,
+        interactive: true
+      }, opts);
+
+    ["files", "exts"].forEach(function( k ) {
+      var v = conf[k];
+
+      if ( v !== "" ) {
+        conf[k] = v.split(",");
+      }
+    });
+
+    ["deep", "interactive", "qiniu", "wantu"].forEach(function( c ) {
+      if ( opts[c] === "false" ) {
+        conf[c] = false;
+      }
+    });
+
+    rocketz.init(conf);
+
+    if ( rocketz.preview() ) {
+      rocketz.run();
+    }
+  }
+});
+
+cmd.run(args[0], args.slice(1), cli.flags);
